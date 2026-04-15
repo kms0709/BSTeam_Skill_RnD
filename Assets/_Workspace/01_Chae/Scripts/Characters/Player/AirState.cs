66a36965f4578a5f8ff1de7fed2e45774412dddc
kms0709 <tkqmskzm@gmail.com>
2026-04-15 21:09:01 +0900

    feat : 폭발 잔해 추가
    폭발하는 잔해 추가 및 프리팹 변환

--- Assets/_Workspace/01_Chae/Scripts/Characters/Player/AirState.cs
+++ Assets/_Workspace/01_Chae/Scripts/Characters/Player/AirState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 
 public class AirState : IState
 {
