820a9bda68020050b883d8784b8c1c25dca0110d
shoes235 <shoes235@outlook.kr>
2026-04-16 22:47:01 +0900

    refactor : 조작변경
    InputGetkey 에서 GetkeyDown으로 변경 : 점프 및 대쉬

---
+++ Assets/_Workspace/01_Chae/Scripts/Characters/Player/GroundState_PLAYER.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class GroundState_PLAYER : IState
+{
+    Player player;
+
+    public GroundState_PLAYER(Player player){
+        this.player = player;
+    }
+    public void Enter(){
+        Debug.Log("OnGround");
+        player.SetGravity(0);
+
+
+        //벽점프 Var 조정
+        player.isWallJumping = false;
+        player.lastWallDir = 0;
+
+        //대쉬 boolean 조정
+        player.isDashing = false;
+        player.canDash = true;
+
+    }
+    public void Update(){
+        player.Move();
+        if(player.IsJumped()){
+            player.Jump();
+        }
+    }
+    public void Exit(){
+        
+    }
+}
--- Assets/_Workspace/01_Chae/Scripts/Characters/Player/Player.cs
+++ Assets/_Workspace/01_Chae/Scripts/Characters/Player/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading;
+using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,105 +10,76 @@ using UnityEngine.UIElements;
 
 public class Player : CharacterParent
 {   
-    //Value vars
-    [SerializeField] private float gravityModifier = 1f;
-    public float gravity = 5f;
-    public float maxFallSpeed = 20f;
-    public float jumpForce;
-    //wallJump Vars
-    [Header("=== Wall Jump Vars ===")]
+    //Player Only
+    public float jumpForce; 
+    
+    [Header("=== Wall Jump Logic ===")]
+    
+    [Header("-01_Force Value")]
+    public float slideSpeed;
+
+    [Header("-02_Times")]
     public float wallAttachTime;
     public float wallJumpDelayTime;
 
-    //public float wallJumpCoolTime;
-    [HideInInspector] public bool isWallJumping;
-    //public bool canWallJump;
-    public float slideSpeed;
+    [HideInInspector] public float lastWallDir;
     
-    [Header("=== Dash Vars ===")]
+    //boolean
+    [HideInInspector] public bool isWallJumping;
+    
+    [Header("=== Dash Logic ===")]
     public float dashForce;
     public float dashDelayTime;
     [HideInInspector] public bool canDash;
     [HideInInspector] public bool isDashing;
 
-
-    //Get componet Vars
-    [HideInInspector] public Rigidbody2D rb;
-
-    [Header("=== States ===")]
-    public Vector2 dir;
-    //States
-    enum PlayerStateType{
-        ON_GROUND,
-        ON_AIR,
-        ON_WALL
-    }
-    [SerializeField] PlayerStateType currentStateType;
-    IState currentState;
-
-    
-    [Header("=== Layers ===")] 
-    //Layers Vars <- Inspector 에서 받음
-    public LayerMask groundLayer;
-    public LayerMask wallLayer;
-    public float rayDistance = 1.0f;
-
-
-    [Header("=== key Binding ===")]
+    [Header("=== Inputs ===")]
     //Key binding
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode dashKey = KeyCode.LeftShift;
-
+    public float inputX;
     //Main
-    private void Awake(){
-        rb = GetComponent<Rigidbody2D>();
-    }
     private void Start(){
         rb.gravityScale = 0;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
-    void Update(){
+    protected override void Update(){
+        base.Update();
         MyInput();
-        UpdateState();
-        currentState?.Update();
     }
-
-    //State Manager Funcs
-    public void ChangeState(IState newState){
-        currentState?.Exit();
-        currentState = newState;
-        currentState.Enter();
+    protected override void FixedUpdate(){
+        // 물리 연산은 이곳에서 발생하더라도 입력은 Update에서 받아야 키 씹힘이나 무한 반복이 발생하지 않습니다.
     }
-
-    void UpdateState(){
-        PlayerStateType newState;
+    protected override void UpdateState(){
+        StateTypePhysics newState;
         //상태구별 후 새상태 부여
         if(IsGrounded()){
-            newState = PlayerStateType.ON_GROUND;
+            newState = StateTypePhysics.ON_GROUND;
         }else if(IsOnWall() && !IsGrounded()){
-            newState = PlayerStateType.ON_WALL;
+            newState = StateTypePhysics.ON_WALL;
         }else{
-            newState = PlayerStateType.ON_AIR;
+            newState = StateTypePhysics.ON_AIR;
         }
         //새상태 체크 후 현재 상태를 새상태로 바꿈
-        if(currentState == null || currentStateType != newState){
+        if(currentPhysicsState == null || currentPhysicsStateType != newState){
             switch(newState){
-                case PlayerStateType.ON_GROUND:
-                    ChangeState(new GroundState(this));
+                case StateTypePhysics.ON_GROUND:
+                    ChangeState(new GroundState_PLAYER(this));
                     break;
-                case PlayerStateType.ON_WALL:
-                    ChangeState(new WallState(this));
+                case StateTypePhysics.ON_WALL:
+                    ChangeState(new WallState_PLAYER(this));
                     break;
-                case PlayerStateType.ON_AIR:
-                    ChangeState(new AirState(this));
+                case StateTypePhysics.ON_AIR:
+                    ChangeState(new AirState_PLAYER(this));
                     break;
             }
-            currentStateType = newState;
+            currentPhysicsStateType = newState;
         }
     }
-
     //Input Funcs
+    //VI
     void MyInput(){
-        dir.x = Input.GetAxis("Horizontal");
+        inputX = Input.GetAxis("Horizontal");
         if(IsDashed() && canDash){
             Debug.Log("Dash!");
             Dash();
@@ -114,49 +87,65 @@ public class Player : CharacterParent
     }
     //--01_Get Input boolean Funcs
     public bool IsJumped(){
-        return Input.GetKey(jumpKey);
+        return Input.GetKeyDown(jumpKey);
     }
     public bool IsDashed(){
-        return Input.GetKey(dashKey);
+        return Input.GetKeyDown(dashKey);
     }
 
     //Physics Funcs
+    //@V_M
     public override void Move(){
-        if(!isWallJumping && !isDashing)
-            rb.velocity = new Vector2(dir.x * moveSpeed,rb.velocity.y);
+        if(inputX != 0 && !isDashing){
+            dirX = Mathf.Sign(inputX);
+        }
+        if(!isWallJumping && !isDashing){
+            rb.velocity = new Vector2(inputX * moveSpeed,rb.velocity.y);
+        }
+        if(!IsGrounded()){
             ApplyGravity();
+        }
+
     }
-    public void SetDefault(){
-        // rb.gravityScale = gravity;
+    public void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x,jumpForce);
     }
-    public void ApplyGravity(){
-        if(gravityModifier == 0) return;
-        float newVelocity = rb.velocity.y - (gravity * gravityModifier * Time.fixedDeltaTime);
-        newVelocity = Mathf.Max(newVelocity,-maxFallSpeed);
-        rb.velocity = new Vector2(rb.velocity.x,newVelocity);
-    }
-    public void SetGravity(float modi){
-        gravityModifier = modi;
-    }
+
+
     //--01_Wall Jump Funcs
+    //@V_W
     public void SlideWall(){
         SetGravity(0);
-        //삼항연산자 -> 현재 방향(입력 방향)과 벽방향이 일치? T : velocity.x 의 값을 0으로 만듬 / F : 그대로 움직임
-        // T -> X가 0이아니면 벽에 달라붙어서 안움직여서,velocity.y -= slideSpped 가 동작이안됌;
-        // F -> 그대로 움직입니다 : 벽에서 탈출;
-        float velX = IsOnWall()? 0 : rb.velocity.x; 
+
+        //벽에 달라붙어있다면 -> Vel.x를 0 으로설정 아니면 벽에서 빠져나옴;
+        // float velX = IsOnWall()? 0 : rb.velocity.x;
         float smooth = Mathf.Lerp(rb.velocity.y, -slideSpeed,Time.deltaTime);
-        rb.velocity = new Vector2(velX,smooth);
+        rb.velocity = new Vector2(0,smooth);
     }
 
     public void AttatchWall(){
         SetGravity(0);
-        float velX = IsOnWall()? 0 : rb.velocity.x;
-        rb.velocity = new Vector2(velX,0);
+        // float velX = IsOnWall()? 0 : rb.velocity.x;
+        rb.velocity = new Vector2(0,0);
     }
+    // public void EscapeWall(){
+    //     SetGravity(1);
+    //     rb.velocity = new Vector2(-dir.x * moveSpeed * 0.1f, jumpForce * 0.1f);
+    // }
     public void JumpWall(){
-        rb.velocity = new Vector2(-Mathf.Sign(dir.x) * moveSpeed * .9f, jumpForce);
+
+        SetGravity(1);
+
+        //현재 벽 방향을 저장;
+        lastWallDir = dirX;
+        
+        //물리 적용
+        rb.velocity = new Vector2(-dirX * moveSpeed * 0.5f, jumpForce);
+        
+        //Set boolean
         isWallJumping = true;
+
+        //Invoke 점프딜레이타임후 isWallJumping -> false
         Invoke(nameof(TriggerWallJump),wallJumpDelayTime);
     }
     public void TriggerWallJump(){
@@ -172,6 +161,9 @@ public class Player : CharacterParent
         //마우스 좌표값 구하기
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
+        if(mousePos.x != 0){
+            dirX = (mousePos.x > 0)? 1f : -1f;
+        }
         Vector2 dirDash = (mousePos - transform.position).normalized;
 
         //물리 작용
@@ -180,19 +172,11 @@ public class Player : CharacterParent
     } 
     void TriggerDash(){
         isDashing = false;
+        // 대시가 끝난 후 위로 계속 날아가는 관성을 제거하여 가로대시와 이동 거리를 맞춤
+        rb.velocity = new Vector2(0f, Mathf.Min(rb.velocity.y, 0f));
     }
     
-    //boolean Funcs
-    public bool IsGrounded(){ // 지면확인
-        RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down,rayDistance,groundLayer);
-        Debug.DrawRay(transform.position, Vector2.down * hit.distance, Color.red);
-        return hit.collider != null;
-    }
-    public bool IsOnWall(){ // 벽 확인
-        RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.right * Mathf.Sign(dir.x) ,rayDistance,wallLayer);
-        Debug.DrawRay(transform.position, Vector2.right * dir.x * hit.distance, Color.blue);
-        return hit.collider != null;
-    }
+
     protected override void Attack(){
         //애니메이션 함수 -> 애니메이션 내부에서 attack area 구현 후 area script에서 부모 atk를 가져와서 처리
     }
